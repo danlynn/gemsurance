13f08cecf9cf53a6327d1646ac55015868c33c1b
Dan Lynn <gitkraken@danlynn.org>
2019-10-17 14:45:12 -0400

    Gracefully skip gems that can't be retrieved from gem repositories

--- lib/gemsurance/gem_info_retriever.rb
+++ lib/gemsurance/gem_info_retriever.rb
@@ -119,31 +119,35 @@ module Gemsurance
       gem_infos = []
 
       @specs.each do |current_spec|
-        active_spec = @bundle_definition.index[current_spec.name].sort_by { |b| b.version }
+        begin
+          active_spec = @bundle_definition.index[current_spec.name].sort_by { |b| b.version }
 
-        if !current_spec.version.prerelease? && !options[:pre] && active_spec.size > 1
-          active_spec = active_spec.delete_if { |b| b.respond_to?(:version) && b.version.prerelease? }
-        end
+          if !current_spec.version.prerelease? && !options[:pre] && active_spec.size > 1
+            active_spec = active_spec.delete_if { |b| b.respond_to?(:version) && b.version.prerelease? }
+          end
 
-        active_spec = active_spec.last
-        next if active_spec.nil?
+          active_spec = active_spec.last
+          next if active_spec.nil?
 
-        gem_outdated = Gem::Version.new(active_spec.version) > Gem::Version.new(current_spec.version)
-        git_outdated = current_spec.git_version != active_spec.git_version
+          gem_outdated = Gem::Version.new(active_spec.version) > Gem::Version.new(current_spec.version)
+          git_outdated = current_spec.git_version != active_spec.git_version
 
-        info = ::Gems.info(active_spec.name)
-        homepage_uri      = info['homepage_uri']
-        documentation_uri = info['documentation_uri']
-        source_code_uri   = info['source_code_uri']
+          info = ::Gems.info(active_spec.name)
+          homepage_uri = info['homepage_uri']
+          documentation_uri = info['documentation_uri']
+          source_code_uri = info['source_code_uri']
 
-        # TODO: handle git versions
-        # spec_version    = "#{active_spec.version}#{active_spec.git_version}"
-        # current_version = "#{current_spec.version}#{current_spec.git_version}"
-        in_gem_file = @dependencies.any?{|d| d.name == active_spec.name}
-        if gem_outdated || git_outdated
-          gem_infos << GemInfo.new(active_spec.name, current_spec.version, active_spec.version, in_gem_file, homepage_uri, documentation_uri, source_code_uri, GemInfo::STATUS_OUTDATED)
-        else
-          gem_infos << GemInfo.new(active_spec.name, current_spec.version, current_spec.version, in_gem_file, homepage_uri, documentation_uri, source_code_uri)
+          # TODO: handle git versions
+          # spec_version    = "#{active_spec.version}#{active_spec.git_version}"
+          # current_version = "#{current_spec.version}#{current_spec.git_version}"
+          in_gem_file = @dependencies.any? { |d| d.name == active_spec.name }
+          if gem_outdated || git_outdated
+            gem_infos << GemInfo.new(active_spec.name, current_spec.version, active_spec.version, in_gem_file, homepage_uri, documentation_uri, source_code_uri, GemInfo::STATUS_OUTDATED)
+          else
+            gem_infos << GemInfo.new(active_spec.name, current_spec.version, current_spec.version, in_gem_file, homepage_uri, documentation_uri, source_code_uri)
+          end
+        rescue => e
+          STDERR.puts "=== skipped processing of spec: #{current_spec.name} - #{e.message}"
         end
       end
       gem_infos
